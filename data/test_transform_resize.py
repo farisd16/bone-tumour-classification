bb7a29f345bf655d8256899355d9c575f36667af
Faris Demirovic <farisdemirovic16@gmail.com>
2025-10-27 15:44:11 +0100

    Add script that applies resize transform

---
+++ data/test_transform_resize.py
@@ -0,0 +1,29 @@
+import os
+from PIL import Image
+from torchvision import transforms
+from tqdm import tqdm
+
+base_dir = os.path.dirname(__file__)
+input_folder = os.path.join(base_dir, "patched_BTXRD")
+output_folder = os.path.join(base_dir, "resized_BTXRD")
+target_size = (224, 224)
+
+os.makedirs(output_folder, exist_ok=True)
+
+resize_transform = transforms.Compose(
+    [
+        transforms.Resize(target_size),
+        # Optional, should retain aspect ratio: transforms.CenterCrop(224)
+    ]
+)
+
+for filename in tqdm(os.listdir(input_folder), desc="Resizing images"):
+    input_path = os.path.join(input_folder, filename)
+    output_path = os.path.join(output_folder, filename)
+
+    try:
+        img = Image.open(input_path)
+        resized_img = resize_transform(img)
+        resized_img.save(output_path)
+    except Exception as e:
+        print(f"Error processing {filename}: {e}")
