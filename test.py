7ca6a90fe11e4545bb8a78b18c2a2f3ca7384de6
Faris Demirovic <farisdemirovic16@gmail.com>
2025-11-10 12:02:55 +0100

    Integrate wandb, fix transforms for validation dataset, add missing dependencies to requirements.txt

--- test.py
+++ test.py
@@ -1,62 +1,76 @@
 import os
+import json
+
 import torch
 import torch.nn as nn
 from torchvision import transforms, models
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from tqdm import tqdm
 from data.custom_dataset_class import CustomDataset
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import (
+    confusion_matrix,
+    f1_score,
+    precision_score,
+    accuracy_score,
+    recall_score,
+    balanced_accuracy_score,
+)
 import seaborn as sns
 import matplotlib.pyplot as plt
-from torch.utils.data import Subset
-import json 
+import wandb
 
 
-# Device 
+# Device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-# Paths 
+# Paths
 base_dir = "checkpoints"
-run_dir = os.path.join(base_dir, "run_2025-11-08_19-58-56")  
+# TODO: Replace hardcoding with CLI argument
+run_name = "resnet_2025-11-10_11-02-01"
+run_dir = os.path.join(base_dir, run_name)
 best_model_path = os.path.join(run_dir, "best_model.pth")
 
-# Transformations (no augmentation, only normalization) 
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5], std=[0.5]),
-])
-
-# Dataset 
-dataset = CustomDataset(
-    image_dir="/Users/bartu/Desktop/Bartu/RCI/3.Semester/ADLM/bone-tumour-classification/data/dataset/patched_BTXRD",
-    json_dir="/Users/bartu/Desktop/Bartu/RCI/3.Semester/ADLM/bone-tumour-classification/data/dataset/BTXRD/Annotations",
-    transform=transform
+# Transformations (no augmentation, only normalization)
+transform = transforms.Compose(
+    [
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        # TODO: Adjust the mean and std to match pretrained dataset
+        transforms.Normalize(mean=[0.5], std=[0.5]),
+    ]
 )
 
-# Load split indices from training 
-with open(os.path.join(run_dir,"data_split.json"), "r") as f:
+# Dataset
+dataset_folder_path = os.path.join("data", "dataset")
+dataset = CustomDataset(
+    image_dir=os.path.join(dataset_folder_path, "patched_BTXRD"),
+    json_dir=os.path.join(dataset_folder_path, "BTXRD", "Annotations"),
+    transform=transform,
+)
+
+# Load split indices from training
+with open(os.path.join(run_dir, "data_split.json"), "r") as f:
     split_indices = json.load(f)
 
 test_indices = split_indices["test"]
 
-# Create test subset 
+# Create test subset
 test_dataset = Subset(dataset, test_indices)
 test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 
-# Model 
-model = models.resnet34(weights=None)  
+# Model
+model = models.resnet34(weights=None)
 model.fc = nn.Sequential(
     nn.Dropout(0.5),
     nn.Linear(512, 7),
 )
 model.load_state_dict(torch.load(best_model_path, map_location=device))
-model = model.to(device)
+model.to(device)
 model.eval()
 
 
-# Loss and metrics 
+# Loss and metrics
 criterion = nn.CrossEntropyLoss()
 test_loss = 0.0
 correct = 0
@@ -65,6 +79,24 @@ total = 0
 all_preds = []
 all_labels = []
 
+api = wandb.Api()
+runs = api.runs(
+    "faris-demirovic-tum-technical-university-of-munich/bone-tumor-classification",
+    filters={"display_name": run_name},
+)
+if len(runs) == 0:
+    print("Error: No wandb runs found with given name")
+    exit()
+if len(runs) > 1:
+    print("Warning: More than one wandb run found with given name")
+run_id = runs[0].id
+test_run = wandb.init(
+    entity="faris-demirovic-tum-technical-university-of-munich",
+    project="bone-tumor-classification",
+    id=run_id,
+    resume="must",
+    job_type="test",
+)
 
 with torch.no_grad():
     for images, labels in tqdm(test_dataloader, desc="[Testing]"):
@@ -87,8 +119,6 @@ print(f"\nTest Loss: {avg_test_loss:.4f} | Test Accuracy: {test_acc:.2f}%")
 
 
 cm = confusion_matrix(all_labels, all_preds)
-print("\nClassification Report:")
-print(classification_report(all_labels, all_preds, digits=3))
 
 plt.figure(figsize=(7, 5))
 sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
@@ -97,3 +127,27 @@ plt.ylabel("True")
 plt.title("Confusion Matrix")
 plt.tight_layout()
 plt.show()
+
+precision_weighted = precision_score(all_labels, all_preds, average="weighted")
+recall_weighted = recall_score(all_labels, all_preds, average="weighted")
+f1_weighted = f1_score(all_labels, all_preds, average="weighted")
+accuracy = accuracy_score(all_labels, all_preds)
+balanced_accuracy = balanced_accuracy_score(all_labels, all_preds)
+
+print("Weighted Precision:", precision_weighted)
+print("Weighted Recall:", recall_weighted)
+print("Weighted F1:", f1_weighted)
+print("Accuracy:", accuracy)
+print("Balanced Accuracy:", balanced_accuracy)
+
+test_run.log(
+    {
+        "Weighted Precision": precision_weighted,
+        "Weighted Recall": recall_weighted,
+        "Weighted F1": f1_weighted,
+        "Accuracy": accuracy,
+        "Balanced Accuracy": balanced_accuracy,
+    }
+)
+
+test_run.finish()
--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 import os
 import json
+import datetime
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -7,8 +9,8 @@ from torchvision import transforms, models
 from torch.utils.data import DataLoader, random_split
 from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
-from PIL import Image
-import datetime
+import wandb
+
 from data.custom_dataset_class import CustomDataset
 
 
@@ -17,57 +19,71 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 # Folder structure and Tensorboard
-base_dir = "checkpoints"
-os.makedirs(base_dir, exist_ok=True)
+checkpoints_base_dir = "checkpoints"
+os.makedirs(checkpoints_base_dir, exist_ok=True)
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-run_dir = os.path.join(base_dir, f"run_{timestamp}")
+run_dir = os.path.join(checkpoints_base_dir, f"resnet_{timestamp}")
+best_model_path = os.path.join(run_dir, "best_model.pth")
 os.makedirs(run_dir, exist_ok=True)
 
+
+# TensorBoard writer
 writer = SummaryWriter(log_dir=run_dir)
 
-
 # Transformations
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.RandomRotation(degrees=15),    
-    
-    # Color jitter: brightness, contrast, saturation, hue
-    transforms.ColorJitter(
-        brightness=0.2,     # ±20% brightness variation
-        contrast=0.2,       # ±20% contrast variation
-        saturation=0.2,     # ±20% saturation variation
-        hue=0.1             # ±0.1 hue shift
-    ),
-    transforms.RandomPerspective(distortion_scale=0.2, p=0.5),  # Random perspective distortion (scale 0.2, probability 0.5)
-    transforms.GaussianBlur(kernel_size=3),   
-    transforms.ToTensor(),
-        
-    transforms.Normalize(mean=[0.5], std=[0.5])  # Normalize to mean=0 (and std=1 by default)
-])
+train_transform = transforms.Compose(
+    [
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomRotation(degrees=15),
+        # Color jitter: brightness, contrast, saturation, hue
+        transforms.ColorJitter(
+            brightness=0.2,  # ±20% brightness variation
+            contrast=0.2,  # ±20% contrast variation
+            saturation=0.2,  # ±20% saturation variation
+            hue=0.1,  # ±0.1 hue shift
+        ),
+        transforms.RandomPerspective(
+            distortion_scale=0.2, p=0.5
+        ),  # Random perspective distortion (scale 0.2, probability 0.5)
+        transforms.GaussianBlur(kernel_size=3),
+        transforms.ToTensor(),
+        # TODO: Adjust the mean and std to match pretrained dataset
+        transforms.Normalize(
+            mean=[0.5], std=[0.5]
+        ),  # Normalize to mean=0 (and std=1 by default)
+    ]
+)
+
+val_transform = transforms.Compose(
+    [
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        # TODO: Adjust the mean and std to match pretrained dataset
+        transforms.Normalize(
+            mean=[0.5], std=[0.5]
+        ),  # Normalize to mean=0 (and std=1 by default)
+    ]
+)
 
 
 # Dataset
+dataset_folder_path = os.path.join("data", "dataset")
 dataset = CustomDataset(
-    image_dir="/Users/bartu/Desktop/Bartu/RCI/3.Semester/ADLM/bone-tumour-classification/data/dataset/patched_BTXRD",
-    json_dir="/Users/bartu/Desktop/Bartu/RCI/3.Semester/ADLM/bone-tumour-classification/data/dataset/BTXRD/Annotations",
-    transform=transform
+    image_dir=os.path.join(dataset_folder_path, "patched_BTXRD"),
+    json_dir=os.path.join(dataset_folder_path, "BTXRD", "Annotations"),
 )
 
-# Dataset length
-total_len = len(dataset)
-train_size = int(0.8 * total_len)
-val_size   = int(0.10 * total_len)
-test_size  = total_len - train_size - val_size
-
 # Train val test dataset
-train_dataset, val_dataset, test_dataset = random_split(dataset, [train_size, val_size, test_size])
+train_dataset, val_dataset, test_dataset = random_split(dataset, [0.8, 0.1, 0.1])
+train_dataset.transform = train_transform
+val_dataset.transform = val_transform
 
 # Save the split indices
 split_indices = {
     "train": train_dataset.indices,
     "val": val_dataset.indices,
-    "test": test_dataset.indices
+    "test": test_dataset.indices,
 }
 
 split_path = os.path.join(run_dir, "data_split.json")
@@ -77,26 +93,40 @@ with open(split_path, "w") as f:
 # Train val test dataloader
 train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 val_dataloader = DataLoader(val_dataset, batch_size=16, shuffle=False)
-test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False)
-
 
 # Model
 model = models.resnet34(weights=models.ResNet34_Weights.IMAGENET1K_V1)
 model.fc = nn.Sequential(
     nn.Dropout(0.5),
-    nn.Linear(512, 7),  
+    nn.Linear(512, 7),
 )
+model.to(device)
 
 # Loss, Optimizere, Scheduler
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-5)
+lr = 1e-4
+weight_decay = 1e-5
+optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
     optimizer, mode="min", factor=0.5, patience=2
 )
 
 # Training loop
-num_epochs = 20
+num_epochs = 5
 best_val_acc = 0.0
+
+# WandB run
+run = wandb.init(
+    entity="faris-demirovic-tum-technical-university-of-munich",
+    project="bone-tumor-classification",
+    config={
+        "learning_rate": lr,
+        "weight_decay": weight_decay,
+        "architecture": "ResNet34",
+        "epochs": num_epochs,
+    },
+    name=f"resnet_{timestamp}",
+)
 
 for epoch in range(num_epochs):
     model.train()
@@ -104,7 +134,9 @@ for epoch in range(num_epochs):
     correct = 0
     total = 0
 
-    for images, labels in tqdm(train_dataloader, desc=f"Epoch {epoch+1}/{num_epochs} [Train]"):
+    for images, labels in tqdm(
+        train_dataloader, desc=f"Epoch {epoch + 1}/{num_epochs} [Train]"
+    ):
         images, labels = images.to(device), labels.to(device)
         optimizer.zero_grad()
         outputs = model(images)
@@ -120,13 +152,14 @@ for epoch in range(num_epochs):
     train_acc = 100 * correct / total
     avg_train_loss = train_loss / len(train_dataloader)
 
-    # === Validation ===
     model.eval()
     val_loss = 0.0
     val_correct, val_total = 0, 0
 
     with torch.no_grad():
-        for images, labels in tqdm(val_dataloader, desc=f"Epoch {epoch+1}/{num_epochs} [Val]"):
+        for images, labels in tqdm(
+            val_dataloader, desc=f"Epoch {epoch + 1}/{num_epochs} [Val]"
+        ):
             images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             loss = criterion(outputs, labels)
@@ -134,16 +167,18 @@ for epoch in range(num_epochs):
             _, predicted = outputs.max(1)
             val_total += labels.size(0)
             val_correct += predicted.eq(labels).sum().item()
-    
+
     val_acc = 100 * val_correct / val_total
     avg_val_loss = val_loss / len(val_dataloader)
 
-    scheduler.step(avg_val_loss)    
+    scheduler.step(avg_val_loss)
 
-    # Logging 
-    print(f"Epoch [{epoch+1}/{num_epochs}] "
-          f"Train Loss: {avg_train_loss:.4f} | Train Acc: {train_acc:.2f}% "
-          f"| Val Loss: {avg_val_loss:.4f} | Val Acc: {val_acc:.2f}%")
+    # Logging
+    print(
+        f"Epoch [{epoch + 1}/{num_epochs}] "
+        f"Train Loss: {avg_train_loss:.4f} | Train Acc: {train_acc:.2f}% "
+        f"| Val Loss: {avg_val_loss:.4f} | Val Acc: {val_acc:.2f}%"
+    )
 
     # Log to TensorBoard
     writer.add_scalar("Loss/Train", avg_train_loss, epoch)
@@ -151,11 +186,28 @@ for epoch in range(num_epochs):
     writer.add_scalar("Accuracy/Train", train_acc, epoch)
     writer.add_scalar("Accuracy/Val", val_acc, epoch)
 
+    # Log to WandB
+    run.log(
+        {
+            "Loss/Train": avg_train_loss,
+            "Loss/Val": avg_val_loss,
+            "Accuracy/Train": train_acc,
+            "Accuracy/Val": val_acc,
+        }
+    )
+
     # Save best model
     if val_acc > best_val_acc:
         best_val_acc = val_acc
-        torch.save(model.state_dict(), f"{run_dir}/best_model.pth")
+
+        torch.save(model.state_dict(), best_model_path)
         print("✅ Saved new best model")
 
+artifact = wandb.Artifact(name=f"resnet_{timestamp}", type="model")
+artifact.add_file(best_model_path)
+artifact.add_file(split_path)
+run.log_artifact(artifact)
+
 writer.close()
+run.finish()
 print("Training complete")
