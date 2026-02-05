f9e42355c75b0313e072a4e7c7bfbffee57aeb2f
Faris Demirovic <farisdemirovic16@gmail.com>
2026-02-05 22:05:39 +0100

    Adjust generation script to support prompt controlling

--- latent_diffusion_finetuned/generate_augmentation_images.sh
+++ latent_diffusion_finetuned/generate_augmentation_images.sh
@@ -18,16 +18,23 @@ conda deactivate
 # If the following does not work, try 'source activate <env-name>'
 conda activate bone-tumour-classification
 # Run the program
-# Usage: sbatch generate_augmentation_images.sh <model_base> <lora_model_path> <num_images> <tumor_subtype>
-# Example: sbatch generate_augmentation_images.sh stable-diffusion /path/to/lora/weights 100 osteofibroma
-MODEL_BASE=${1:?"Error: MODEL_BASE not provided. Usage: sbatch generate_augmentation_images.sh <model_base> <lora_model_path> <num_images> <tumor_subtype>"}
-LORA_MODEL_PATH=${2:?"Error: LORA_MODEL_PATH not provided. Usage: sbatch generate_augmentation_images.sh <model_base> <lora_model_path> <num_images> <tumor_subtype>"}
-NUM_IMAGES=${3:?"Error: NUM_IMAGES not provided. Usage: sbatch generate_augmentation_images.sh <model_base> <lora_model_path> <num_images> <tumor_subtype>"}
-TUMOR_SUBTYPE=${4:?"Error: tumor_subtype not provided. Usage: sbatch generate_augmentation_images.sh <model_base> <lora_model_path> <num_images> <tumor_subtype>"}
+# Usage: sbatch generate_augmentation_images.sh <model_base> <lora_model_path> <num_images> <tumor_subtype> [use_detailed_prompt]
+# Example: sbatch generate_augmentation_images.sh stable-diffusion /path/to/lora/weights 100 osteofibroma false
+MODEL_BASE=${1:?"Error: MODEL_BASE not provided. Usage: sbatch generate_augmentation_images.sh <model_base> <lora_model_path> <num_images> <tumor_subtype> [use_detailed_prompt]"}
+LORA_MODEL_PATH=${2:?"Error: LORA_MODEL_PATH not provided. Usage: sbatch generate_augmentation_images.sh <model_base> <lora_model_path> <num_images> <tumor_subtype> [use_detailed_prompt]"}
+NUM_IMAGES=${3:?"Error: NUM_IMAGES not provided. Usage: sbatch generate_augmentation_images.sh <model_base> <lora_model_path> <num_images> <tumor_subtype> [use_detailed_prompt]"}
+TUMOR_SUBTYPE=${4:?"Error: tumor_subtype not provided. Usage: sbatch generate_augmentation_images.sh <model_base> <lora_model_path> <num_images> <tumor_subtype> [use_detailed_prompt]"}
+USE_DETAILED_PROMPT=${5:-"false"}  # Default to false if not provided
+
+# Build the detailed prompt flag
+DETAILED_PROMPT_FLAG=""
+if [ "$USE_DETAILED_PROMPT" = "true" ]; then
+    DETAILED_PROMPT_FLAG="--use_detailed_prompt"
+fi
 
 srun python generate_augmentation_images.py \
     --lora_model_path $LORA_MODEL_PATH \
     --model_base $MODEL_BASE \
     --num_images $NUM_IMAGES \
     --tumor_subtype $TUMOR_SUBTYPE \
-    --use_detailed_prompt
+    $DETAILED_PROMPT_FLAG
